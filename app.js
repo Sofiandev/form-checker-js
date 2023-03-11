9a3d9be9d0a53fadf405e77affeee33676588cd8
SofianAcDev <sofian.achbabi@gmail.com>
2023-03-11 21:49:08 +0100

    init

---
+++ app.js
@@ -0,0 +1,40 @@
+const inputs = document.querySelectorAll(
+  "input[type='text'], input[type='password']"
+);
+
+const pseudoChecker = (value) => {
+  console.log(value);
+};
+
+const emailChecker = (value) => {
+  console.log(value);
+};
+
+const passwordChecker = (value) => {
+  console.log(value);
+};
+
+const confirmChecker = (value) => {
+  console.log(value);
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "pseudo":
+        pseudoChecker(e.target.value);
+        break;
+      case "email":
+        emailChecker(e.target.value);
+        break;
+      case "password":
+        passwordChecker(e.target.value);
+        break;
+      case "confirm":
+        confirmChecker(e.target.value);
+        break;
+      default:
+        null;
+    }
+  });
+});
